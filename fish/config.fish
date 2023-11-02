ef89e70a2b96acc2037ed599fe7d873e0f47ef3d
Craig J. Wessel <cwessel@marguerite.local>
2023-11-01 17:42:21 -0700

    initial commit, fish dotfiles and neofetch dotfiles as of 11-1-2023

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+eval "$(oh-my-posh init fish --config $(brew --prefix oh-my-posh)/themes/craver.omp.json)"
+alias ls="lsd"
+alias df="duf"
+alias man="tldr"
+neofetch
+end
