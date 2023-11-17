dd9e8e1a1e6fc47cbffa96c5af8812796b683cba
Craig J. Wessel <craigwessel9@gmail.com>
2023-11-16 19:52:00 -0800

    updating apt function in the dotfiles to better use the nala wrapper round 2

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,17 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+export MOZ_ENABLE_WAYLAND=1
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 eval "$(oh-my-posh init fish --config $(brew --prefix oh-my-posh)/themes/craver.omp.json)"
 alias ls="lsd"
 alias df="duf"
 alias man="tldr"
-alias apt="sudo nala"
+function sudo
+        if [ "$argv[1]" = "apt" ]
+                command sudo nala $argv[2..-1]
+        else
+                command sudo $argv
+        end
+end
 neofetch
 end
